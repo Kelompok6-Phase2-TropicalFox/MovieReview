cb61296ec10c23b3fa3befaf3391e0a041b0b81d
Naufal Muhamad Siddiq <obostea12@gmail.com>
2020-09-03 23:33:46 +0700

    OauthAPIGoogle

--- server/app.js
+++ server/app.js
@@ -2,9 +2,9 @@ require ("dotenv").config ()
 
 const express = require('express')
 const routes = require('./routes/index')
-
+console.log(process.env.CLIENT_ID);
 const app = express()
-const port = 3000
+const port = 3100
 
 app.use(express.urlencoded({extended:false}))
 app.use (express.json ())
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -44,9 +44,48 @@ class UserController {
             return res.status (201).json (data)
         })
         .catch (err => {
+            console.log(err);
             return res.status (500).json ({message : "Internal Server Error"})
         })
     }
+
+    static googleLogin(req, res, next){
+        const client = new OAuth2Client(process.env.CLIENT_ID)
+        const { google_access_token } = req.headers;
+    
+        let email_google = ''
+    
+        client.verifyIdToken({
+          idToken:google_access_token,
+          audience:process.env.CLIENT_ID
+        })
+        .then(ticket => {
+          return ticket.getPayload()
+        })
+        .then(payload =>{
+          email_google = payload.email;
+          return User.findOne({where: payload.email})
+        })
+        .then(user =>{
+          if(!user){
+            let userObj = {
+              email : email_google,
+              password: "rahasia",
+            }
+          }else{
+            return user;
+          }
+        })
+        .then(data => {
+          const payload = { email:data.email, id : data.id};
+          const access_token = signToken(payload)
+    
+          return res.status(200).json({access_token})
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      }    
 }
 
 module.exports = UserController
